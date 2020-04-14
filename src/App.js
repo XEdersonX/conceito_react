71b391f0827913b2da2a3c4ef91b0446a108c713
Ederson Luís <edergp2009@gmail.com>
2020-04-14 10:51:40 -0300

    Utilizando a importacao de css e imagens.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React, { useState } from 'react';
 
+import './App.css';
+import backgroundImage from './assets/backgroud.jpg';
+
 import Header from './components/Header';
 
 /**
@@ -36,6 +39,8 @@ function App() {
     //fragments
     <>
       <Header title="Projects" />
+
+      <img width={300} src={backgroundImage}/>
 
       <ul>
         {projects.map(project => <li key={project}>{project}</li>)}
