9d7446c014e9e7fae5f86883a953665f34457ce8
Ederson Luís <edergp2009@gmail.com>
2020-04-14 08:52:03 -0300

    Utilizando propriedades

--- src/App.js
+++ src/App.js
@@ -2,13 +2,32 @@ import React from 'react';
 
 import Header from './components/Header';
 
+/**
+ * 3 CONCEITOS MAIS IMPORTANTES DO REACT:
+ * 
+ * Componente
+ * Propriedade
+ * Estado & Imutabilidade
+ */
+
 //Criando função de componentizacao
 function App() {
   return(
     //fragments
     <>
-      <Header />
-      <Header />
+      <Header title="Homepage">
+        <ul>
+          <li>Homepage</li>
+          <li>Projects</li>
+        </ul>
+      </Header>
+      <Header title="Projects">
+        <ul>
+          <li>Homepage</li>
+          <li>Projects</li>
+          <li>Login</li>
+        </ul>
+      </Header>
     </>
   );
 }
